b4b7bf58b0ff36131a8d8a0a50e2a102ccd94cf9
Zümra ALKAN <96260092+zmrlkn@users.noreply.github.com>
2024-03-16 19:55:42 +0300

    Character controller
    animasyon geçişlerinde düzeltme yapılması gerek . zıplama ve saldırı eklenmedi

--- 2DGameJam/Assets/Scripts/characterController.cs
+++ 2DGameJam/Assets/Scripts/characterController.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float hareketHizi = 5f; // Karakterin hareket hızı
+    private Animator animator; // Animator bileşeni
+    private SpriteRenderer spriteRenderer; // SpriteRenderer bileşeni
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>(); // Animator bileşenini al
+        spriteRenderer = GetComponent<SpriteRenderer>(); // SpriteRenderer bileşenini al
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HareketKontrolleri();
+    }
+
+    void HareketKontrolleri()
+    {
+        float yatayHareket = Input.GetAxis("Horizontal"); // Sağ-sol tuşlarına karşılık gelen girişi al
+
+        // Yukarı-Assagı tuşlarına karşılık gelen girişi al
+        // Bu kısmı space tuşuna basılma kontrolü için değiştirdik
+        float dikeyHareket = Input.GetKey(KeyCode.W) ? 1f : 0f;
+
+        Vector3 hareket = new Vector3(yatayHareket, 0f, 0f); // Hareket vektörü oluştur
+
+        Vector3 ziplama = new Vector3(0f, dikeyHareket, 0f); // Hareket vektörü oluştur
+
+        transform.position += hareket * hareketHizi * Time.deltaTime; // Hareketi uygula
+
+        transform.position += ziplama * 7f * Time.deltaTime; // Zıplama hareketi uygula
+        if (dikeyHareket != 0f)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+
+        // Run animasyonunu kontrol et
+        if (yatayHareket != 0f)
+        {
+            animator.SetBool("Run", true); // Hareket ediliyorsa Run'u true yap
+
+            if (yatayHareket < 0f)
+            {
+                spriteRenderer.flipX = true; // Sol tarafa hareket ediliyorsa sprite'ı çevir
+            }
+            else
+            {
+                spriteRenderer.flipX = false; // Sağ tarafa hareket ediliyorsa sprite'ı çevirme
+            }
+        }
+        else
+        {
+            animator.SetBool("Run", false); // Hareket edilmiyorsa Run'u false yap
+        }
     }
 }
+
